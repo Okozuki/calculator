9cc6d3cd23258bd1563df2642c5b17b7a75fcbd0
mahmoud <mahmoud@orange.fr>
2022-05-25 13:19:05 +0200

    Updating test resources

--- src/test/java/com/okozuki/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/okozuki/calculator/CalculatorApplicationTests.java
@@ -3,10 +3,12 @@ package com.okozuki.calculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
+@DirtiesContext
 class CalculatorApplicationTests {
 	@Autowired
 	private Calculator calculator;
